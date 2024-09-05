efb0db75ff4c590d8c114c43f1f312e8b265af94
Daito-Non <0up567503251d4n@au.com>
2024-09-05 19:50:10 +0900

    ゲーム制作（イガグリ投擲＆エフェクト処理）

---
+++ Assets/script/IgaguriGenerator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class IgaguriGenerator : MonoBehaviour
+{
+    public GameObject igaguriPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            GameObject igaguri=Instantiate(igaguriPrefab);
+            igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+        }
+    }
+}
