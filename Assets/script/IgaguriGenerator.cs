9ff2abd8490bf3374e19d7923f0ac728bf386468
606-12\student <0up567503251d4n@au.com>
2024-09-12 19:47:33 +0900

    スコア表示

--- Assets/script/IgaguriGenerator.cs
+++ Assets/script/IgaguriGenerator.cs
@@ -15,10 +15,13 @@ public class IgaguriGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            GameObject igaguri=Instantiate(igaguriPrefab);
-            igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));  
+            GameObject igaguri = Instantiate(igaguriPrefab);
+            //igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(worldDir.normalized * 2000);
         }
     }
 }
